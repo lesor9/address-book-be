5f4c1a595756291e18451d1462288ef436dd8d3f
lesor9 <lesor@protonmail.com>
2021-03-06 19:29:45 +0300

    fix: change cors lib to middleware

--- app.ts
+++ app.ts
@@ -1,6 +1,5 @@
 import * as express from 'express';
 import * as logger from 'morgan';
-import * as cors from 'cors';
 
 import indexRouter from './routes/index';
 import usersRouter from './routes/users'
@@ -11,7 +10,15 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested, Content-Type, Accept Authorization"
+  )
+  next()
+});
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
