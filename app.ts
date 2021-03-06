538d014f3d6d9337d9154eae659d1ec0b210dec5
lesor9 <lesor@protonmail.com>
2021-03-06 19:08:44 +0300

    fix: check if problem with crash is with cors library

--- app.ts
+++ app.ts
@@ -1,13 +1,13 @@
 import * as express from 'express';
 import * as logger from 'morgan';
-import * as cors from 'cors';
+// import * as cors from 'cors';
 
 import indexRouter from './routes/index';
 import usersRouter from './routes/users'
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
