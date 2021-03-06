be0e05bef9ebf274fa00ff86aa00b3dcd64e558e
lesor9 <lesor@protonmail.com>
2021-03-06 19:12:10 +0300

    fix: error with cors

--- app.ts
+++ app.ts
@@ -1,17 +1,17 @@
 import * as express from 'express';
 import * as logger from 'morgan';
-// import * as cors from 'cors';
+import * as cors from 'cors';
 
 import indexRouter from './routes/index';
 import usersRouter from './routes/users'
 
 const app = express();
 
-// app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use(cors());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
